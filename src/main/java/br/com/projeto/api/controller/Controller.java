a284651f1a714a4eeff2fb189cd6ed6513041837
luanmvcosta0 <luanmvcosta@mgail.com>
2024-06-19 22:40:52 -0300

    adicionando rotas put e delete na api

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -8,9 +8,11 @@ import br.com.projeto.api.repository.Repositorio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -33,6 +35,18 @@ public class Controller {
     @GetMapping("/api/{codigo}")
     public Pessoa selecionarPeloCodigo(@PathVariable int codigo) {
         return acao.findByCodigo(codigo);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj) {
+        return acao.save(obj);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo) {
+        Pessoa obj = selecionarPeloCodigo(codigo);
+
+        acao.delete(obj);
     }
 
     @GetMapping("/teste")
